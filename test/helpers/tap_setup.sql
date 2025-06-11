4ba28f4a36d3dd95b1729ff2f04264633fcec966
jnasbyupgrade <jnasby@upgrade.com>
2025-06-11 16:13:59 -0500

    Test actually installing and committing.

--- test/helpers/tap_setup.sql
+++ test/helpers/tap_setup.sql
@@ -1,10 +1,14 @@
 \i test/helpers/psql.sql
 
-CREATE SCHEMA tap;
+/*
+ * NOTE: if you get errors about things already existing it's because they've
+ * been left behind by test/sql/install.sql
+ */
+SET client_min_messages = WARNING;
+CREATE SCHEMA IF NOT EXISTS tap;
 SET search_path = tap;
 CREATE EXTENSION IF NOT EXISTS pgtap SCHEMA tap;
---\i test/helpers/pgtap-core.sql
---\i test/helpers/pgtap-schema.sql
+SET client_min_messages = NOTICE;
 
 \pset format unaligned
 \pset tuples_only true
