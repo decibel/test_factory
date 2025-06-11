b59de00b2b752086ecea7c5859628424493ba8d8
Jim Nasby <jim@nasby.net>
2025-06-11 15:30:28 -0500

    Squashed 'pgxntool/' changes from 48dce67..bed3604
    bed3604 Fix pg_regress on versions > 12 (#5) (#6)
    c0af00f Improvements to HISTORY.asc
    6e8f2a7 Allow use of sudo when installing an extension
    705f1ec Don't run clean as part of make test
    370fa8e Create test/sql during setup
    890053c Fix bug with DOCS
    5c2173e Fix DOCS variable
    d8bf8ed Update HISTOROY
    f92d493 Add support for asciidoc
    dbdd978 Improve asciidoc handling
    90bbfa5 Merge branch 'stable'
    2f11ef0 Support 9.2
    bceb84d Don't over-write DOC
    1b97c8c Add pgtap as an installcheck dependency
    cd24b0f Reduce verbosity of test setup
    
    git-subtree-dir: pgxntool
    git-subtree-split: bed36044679d6b53ad7cd2875272552a4ad6508a

--- test/deps.sql
+++ test/deps.sql
@@ -1,3 +1,15 @@
--- Note: pgTap is loaded by setup.sql
+-- IF NOT EXISTS will emit NOTICEs, which is annoying
+SET client_min_messages = WARNING;
 
 -- Add any test dependency statements here
+-- Note: pgTap is loaded by setup.sql
+--CREATE EXTENSION IF NOT EXISTS ...;
+/*
+ * Now load our extension. We don't use IF NOT EXISTs here because we want an
+ * error if the extension is already loaded (because we want to ensure we're
+ * getting the very latest version).
+ */
+CREATE EXTENSION ...;
+
+-- Re-enable notices
+SET client_min_messages = NOTICE;
--- test/pgxntool/tap_setup.sql
+++ test/pgxntool/tap_setup.sql
@@ -1,8 +1,19 @@
 \i test/pgxntool/psql.sql
 
-CREATE SCHEMA IF NOT EXISTS tap;
+SET client_min_messages = WARNING;
+
+DO $$
+BEGIN
+IF NOT EXISTS(SELECT 1 FROM pg_namespace WHERE nspname='tap') THEN
+  CREATE SCHEMA tap;
+END IF;
+END$$;
+
 SET search_path = tap, public;
 CREATE EXTENSION IF NOT EXISTS pgtap SCHEMA tap;
+SET client_min_messages = NOTICE;
 \pset format unaligned
 \pset tuples_only true
 \pset pager
+
+-- vi: expandtab ts=2 sw=2
